c1832efd42d19c9f81e6805c304be87e61417854
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-08-12 13:16:06 -0300

    feat :sparkles: estudando for e forEach

---
+++ fundamentos/ForEForEach/Program.cs
@@ -0,0 +1,17 @@
+﻿﻿public class Program
+{
+    public static void Main()
+    {
+        string[] names = new string[] { "Hulk", "Thor", "Loki" };
+
+        //for(int i = 0; i < names.Length; i++) {
+        //    Console.WriteLine(names[i]);
+        //}
+        
+        foreach (var name in names)
+        {
+            Console.WriteLine(name);
+        }
+    }
+
+}
