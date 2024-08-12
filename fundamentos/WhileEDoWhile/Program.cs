9d1e9f5848a0da79757a41670613383e29621809
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-08-12 13:11:04 -0300

    feat :sparkles: estudando while e do while

---
+++ fundamentos/WhileEDoWhile/Program.cs
@@ -0,0 +1,22 @@
+﻿﻿public class Program
+{
+    public static void Main()
+    {
+        var count = 0;
+
+        // while (count < 10
+        // {
+        //     Console.WriteLine("count " + count);
+        //     count++;
+        // }
+        //A diferença do while para o do while é que o do while
+        //executa pelo menos uma vez o código.
+        do
+        {
+            Console.WriteLine("count " + count);
+            count++;
+        }
+        while (count < 10);
+    }
+
+}
