baa52f4556d390ee45d8a91e0c62f2d1fbe90cad
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-08-13 21:15:38 -0300

    feat :sparkles: Estudando try catch

---
+++ fundamentos/TryCatchCSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string[] chemicalProduct = new string[3];
+
+        try
+        {
+            chemicalProduct[0] = "Cálcio";
+            chemicalProduct[1] = "Zinco";
+            chemicalProduct[2] = "Hidrazina";
+            chemicalProduct[3] = "Anilina";
+        }
+        catch (IndexOutOfRangeException ex)
+        {
+            Console.WriteLine("Erro Específico, sabemos exatamente o motivo do erro."); //lança um erro personalizado
+        
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Temos a mensagem, porém é um pouco incerto o que ocorreu.");
+        }
+    }
+}
